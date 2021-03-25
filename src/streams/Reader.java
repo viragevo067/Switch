c64a6aff60df0d03e4b059276b736b852108fa45
viragevo067 <9297188288v@gmail.com>
2021-03-26 01:41:57 +0300

    https://github.com/romankh9/git-demo.git

--- src/streams/Reader.java
+++ src/streams/Reader.java
@@ -3,16 +3,17 @@ package streams;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Reader {
-    public static void main(String[] args) throws FileNotFoundException {
-        String testt = null;
-        File f = new File(testt);
-
-        Scanner sc = new Scanner(testt);
-        sc.nextLine();
-
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("test1");
+        Scanner scan = new Scanner(fr);
+        while (scan.hasNextLine()) {
+            System.out.println(scan.nextLine());
+        }
+        fr.close();
     }
 }
